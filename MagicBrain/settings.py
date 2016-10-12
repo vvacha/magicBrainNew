5cd476271a82dc2af3370724377db403c254fc99
Rienopely <rienopely@mail.ru>
2016-10-12 21:06:40 +1000

    Update settings.py

--- MagicBrain/settings.py
+++ MagicBrain/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i)zz@24d*=3axvo*&qe1)+)%%wyzfk06&3ee6w_mrwn=9e8cos'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
